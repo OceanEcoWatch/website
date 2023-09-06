6f1ca294390f0a90926d51697eace68fb4a9d26f
joostwmd <88150245+joostwmd@users.noreply.github.com>
2023-09-06 20:56:45 +0200

    i18n setup done

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import '../../app.postcss';
+
+	import type { LayoutData } from './$types';
+
+	// import 'normalize.css';
+	// import '$styles/main.scss';
+
+	import { setLocale } from '$i18n/i18n-svelte';
+	import { loadLocale } from '$i18n/i18n-util.sync';
+
+	export let data: LayoutData;
+
+	loadLocale(data.locale);
+	setLocale(data.locale);
 </script>
 
 <slot />
--- src/routes/(app)/[locale]/+page.svelte
+++ src/routes/(app)/[locale]/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import LL from '$i18n/i18n-svelte';
-	console.log($LL.HI(), 'test');
 </script>
 
 <p>{$LL.HI()}</p>
