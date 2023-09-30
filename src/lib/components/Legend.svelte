71375c8781d41a9eb3c028fc802ff22adb3ff1a2
joostwmd <88150245+joostwmd@users.noreply.github.com>
2023-09-30 23:26:01 +0200

    feat:legende

---
+++ src/lib/components/Legend.svelte
@@ -0,0 +1,43 @@
+<div class="flex flex-col items-center bg-white card p-2">
+	<div class="flex sm:space-x-2 md:space-x-5 lg:space-x-5 mb-2">
+		<div>
+			<p class="text-black">10%</p>
+			<div class="w-[30px] aspect-square bg-pollution-100" />
+		</div>
+		<div>
+			<p class="text-black">20%</p>
+			<div class="w-[30px] aspect-square bg-pollution-200" />
+		</div>
+		<div>
+			<p class="text-black">30%</p>
+			<div class="w-[30px] aspect-square bg-pollution-300" />
+		</div>
+		<div>
+			<p class="text-black">40%</p>
+			<div class="w-[30px] aspect-square bg-pollution-400" />
+		</div>
+		<div>
+			<p class="text-black">50%</p>
+			<div class="w-[30px] aspect-square bg-pollution-500" />
+		</div>
+		<div>
+			<p class="text-black">60%</p>
+			<div class="w-[30px] aspect-square bg-pollution-600" />
+		</div>
+		<div>
+			<p class="text-black">70%</p>
+			<div class="w-[30px] aspect-square bg-pollution-700" />
+		</div>
+		<div>
+			<p class="text-black">80%</p>
+			<div class="w-[30px] aspect-square bg-pollution-800" />
+		</div>
+		<div>
+			<p class="text-black">90%</p>
+			<div class="w-[30px] aspect-square bg-pollution-900" />
+		</div>
+	</div>
+	<div>
+		<p class="text-center">probability of present marine debris</p>
+	</div>
+</div>
