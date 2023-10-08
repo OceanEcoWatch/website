ae34b57bf326a7b55551066ceb642f3235215e74
emely3h <emely3h@gmail.com>
2023-10-08 19:06:12 +0200

    minor style changes

--- src/lib/sections/Contact.svelte
+++ src/lib/sections/Contact.svelte
@@ -1,11 +1,11 @@
-<div class="w-full bg-primary-500 p-10">
-	<h1 class="text-2xl text-white">Get our marine debris analysis of any coastal region</h1>
+<div class="w-full bg-primary-500 p-10 pt-28">
+	<h1 class="text-4xl text-white pb-10" style="font-weight: 500">Get our marine debris analysis of any coastal region</h1>
 	<p class="text-white mt-5">
 		Interested in our data? We can provide you with a detailed analysis of any coastal region in the
 		world from 2014 to the present.
 	</p>
 
-	<p class="text-tertiary-500 mt-10">contact@oceanecowatch.org</p>
+	<p class="text-tertiary-500 mt-10" style="font-weight: 500">contact@oceanecowatch.org</p>
 
 	<div class="flex flex-col items-center mt-20">
 		<a class="anchor text-white" href="/impressum"><p>impressum & datanschutz</p></a>
