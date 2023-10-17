2a71734149e82aef9c19b9245f0723aef9746b45
emely3h <emely3h@gmail.com>
2023-10-17 14:44:38 +0200

    polish heor + problem

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -3,23 +3,23 @@
 	import earth from '$assets/images/earth.png';
 </script>
 
-<div class="bg-black p-20 sm:p-10 min-h-screen w-full relative">
+<div class="bg-black lg:p-40 sm:p-0 min-h-screen w-full relative flex items-center">
 	<img
 		class="object-cover sm:object-top absolute top-0 left-0 z-0 w-full h-full"
 		src={earth}
 		alt="globe from space"
 	/>
-	<div class="sm:w-5/6 w-1/2 h-full z-10 relative mt-20 sm:mt-5">
+	<div class="sm:w-5/6 w-1/2 h-full z-20 sm:ml-5 md:ml-10">
 		<h1 class="text-5xl sm:text-4xl text-white">MAPPING THE PLASTIC THREAT</h1>
 
 		<div class="w-2/3 h-[4px] bg-secondary-500 mt-20" />
 
-		<h1 class="text-3xl sm:text-2xl sm:w-2/5 text-secondary-500 mt-20 sm:mt-5">
+		<h1 class="text-4xl sm:text-2xl sm:w-2/5 text-secondary-500 mt-20 sm:mt-5" style="font-weight: 500">
 			REAL-TIME MAPPING OF PRESENT <br />MARINE DEBRIS BY LEVERAGING <br />SATELLITE IMAGERY
 		</h1>
 
-		<button class="btn h-[75px] w-[250px] bg-white mt-20 sm:mt-20" on:click={() => goto('/map')}>
-			<h1 class="text-black text-2xl font-bold">VIEW PROTOTYPE</h1>
+		<button class="btn h-[90px] w-[320px] sm:btn h-[75px] sm:w-[270px] bg-white mt-20 sm:mt-20" style="border-radius: 28px;" on:click={() => goto('/map')}>
+			<h1 class="text-black text-3xl sm:text-2xl font-bold">VIEW PROTOTYPE</h1>
 		</button>
 	</div>
 </div>
--- src/lib/sections/Problem.svelte
+++ src/lib/sections/Problem.svelte
@@ -1,27 +1,29 @@
-<div class="w-full bg-primary-500 flex flex-col items-center p-32 sm:px-5">
-	<div class="w-2/3 sm:w-full text-center text-4xl text-white font-bold">
-		<h1>Plastic pollution threatens the ecosystem, the economy and human health!</h1>
-	</div>
-	<div class="w-4/5 flex sm:flex-col sm:space-y-10 justify-between mt-16">
-		<div class="w-1/4 sm:w-full">
-			<p class="text-white text-center">
-				<span class="text-tertiary-500 font-bold text-3xl">In 2050</span><br class="mb-5" />our
-				oceans could hold more plastic than fish by weight
-			</p>
-		</div>
+<div class="w-full bg-primary-500 flex flex-col items-center lg:py-56 md:py-48 sm:py-32 md:p-30 sm:px-5">
+    <div class="w-1/2 sm:w-4/5 md:w-full text-center text-4xl text-white font-bold lg:px-10">
+        <h1>Plastic pollution threatens the ecosystem, the economy, and human health!</h1>
+    </div>
+    <div class="w-4/5 flex md:flex-col sm:flex-col sm:space-y-20 md:space-y-10 justify-between mt-28 lg:pt-10">
+        <div class="w-1/4 sm:w-full md:w-full sm:pb-5">
+            <p class="text-white text-lg-p text-center">
+                <span class="text-tertiary-500 font-bold text-5xl">In 2050</span><br class="mb-5" />our
+                
+                
+                oceans could hold more plastic than fish by weight
+            </p>
+        </div>
 
-		<div class="w-1/4 sm:w-full">
-			<p class="text-white text-center">
-				<span class="text-tertiary-500 font-bold text-3xl">450</span><br class="mb-5" />years it
-				takes to break down a plastic bottle into microplastic particles.
-			</p>
-		</div>
+        <div class="w-1/4 sm:w-full md:w-full">
+            <p class="text-white text-lg-p text-center sm:pb-5">
+                <span class="text-tertiary-500 font-bold text-5xl">450 years</span><br class="mb-5" />it
+                takes to break down a plastic bottle into microplastic particles.
+            </p>
+        </div>
 
-		<div class="w-1/4 sm:w-full">
-			<p class="text-white text-center">
-				<span class="text-tertiary-500 font-bold text-3xl">$13 billion</span><br class="mb-5" />are
-				the cost of annual damage to marine ecosystems cased by plastic waste.
-			</p>
-		</div>
-	</div>
+        <div class="w-1/4 sm:w-full md:w-full">
+            <p class="text-white text-lg-p text-center sm:pb-5">
+                <span class="text-tertiary-500 font-bold text-5xl">$13 billion</span><br class="mb-5" />are
+                the cost of annual damage to the marine ecosystems caused by plastic pollution.
+            </p>
+        </div>
+    </div>
 </div>
